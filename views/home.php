43202a518316165d124d4b9d81eb8c2bd64e8f2b
NaderHamid9 <nadermh2020@gmail.com>
2024-06-02 12:12:50 +0300

    edited the title of the page

--- views/home.php
+++ views/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks</title>
+    <title>Vero Web Portal</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
